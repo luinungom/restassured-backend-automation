7f0140f61c9800be7582658ede33a74f932eb471
Luis N <luinungom@gmail.com>
2021-04-20 22:45:12 +0200

    First commit

---
+++ src/test/java/com/herokuapp/resfullbooker/GetBookingIdsTests.java
@@ -0,0 +1,40 @@
+package com.herokuapp.resfullbooker;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.*;
+
+import java.util.List;
+
+public class GetBookingIdsTests {
+
+	@Test
+	public void getBookingIdsWithoutFilterTest() {
+		// Get response with booking ids
+		Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking");
+		// Check if response code equals 200
+		Assert.assertEquals(response.statusCode(), 200, "Status code is not 200");
+		// Verify at least 1 booking in response
+		List<Integer> bookingIds = response.jsonPath().getList("bookingid");
+		Assert.assertTrue(bookingIds.size() > 0, "Error, booking list is empty");
+	}
+	
+	@Test
+	public void getBookingWithIdsTest() {
+		// Get response with booking ids
+		Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking/1");
+		// Check if response code equals 200
+		Assert.assertEquals(response.statusCode(), 200, "Status code is not 200");
+		// Check if first name equals Sally
+		String firstName = response.jsonPath().getString("firstname");
+		Assert.assertEquals(firstName, "Sally", "Error, first name does not match the expected one");
+		// Check if last name equals Brown
+		String lastName = response.jsonPath().getString("lastname");
+		Assert.assertEquals(firstName, "Brown", "Error, last name does not match the expected one");
+	}
+
+}
