4a4730ab29cc3fe3cffaba120d782e7eb247702f
Luis N <luinungom@gmail.com>
2021-04-26 21:28:22 +0200

    Test getBookingTest() completed

--- src/test/java/com/herokuapp/resfullbooker/GetBookingTests.java
+++ src/test/java/com/herokuapp/resfullbooker/GetBookingTests.java
@@ -19,23 +19,27 @@ public class GetBookingTests {
 		// Check if response code equals 200
 		Assert.assertEquals(response.statusCode(), 200, "Status code is not 200");
 		
-		// Check if first name equals Sally
+		// Check if first name equals Jim
 		String firstName = response.jsonPath().getString("firstname");
 		SoftAssert softAssert = new SoftAssert();
-		softAssert.assertEquals(firstName, "Sally", "Error, first name does not match the expected one");
-		// Check if last name equals Brown
+		softAssert.assertEquals(firstName, "Jim", "Error, first name does not match the expected one");
+		// Check if last name equals Ericsson
 		String lastName = response.jsonPath().getString("lastname");
-		softAssert.assertEquals(lastName, "Brown", "Error, last name does not match the expected one");
-		// Check if total price equals 743
+		softAssert.assertEquals(lastName, "Ericsson", "Error, last name does not match the expected one");
+		// Check if total price equals 403
 		int totalPrice = response.jsonPath().getInt("totalprice");
-		softAssert.assertEquals(totalPrice, 743, "Total price does not match the expected one");
+		softAssert.assertEquals(totalPrice, 403, "Total price does not match the expected one");
 		// Check if the deposit has been paid
 		boolean depositPaid = response.jsonPath().getBoolean("depositpaid");
 		softAssert.assertEquals(true, depositPaid, "Deposit should be paid but it is not");
-		// Check if check in date is 2015-07-15
+		// Check if checking date is 2016-03-10
 		LocalDate checkingDate = LocalDate.parse(response.jsonPath().getString("bookingdates.checkin"));
-		boolean checkingMatch = (checkingDate.isEqual(LocalDate.parse("2015-05-03")));
-
+		boolean checkingMatch = (checkingDate.isEqual(LocalDate.parse("2016-03-10")));
+		softAssert.assertTrue(checkingMatch, "Checking date is incorrect");
+		// Check if checkout date is 2020-03-31
+		LocalDate checkoutDate = LocalDate.parse(response.jsonPath().getString("bookingdates.checkout"));
+		boolean checkoutMatch = (checkoutDate.isEqual(LocalDate.parse("2020-03-31")));
+		softAssert.assertTrue(checkoutMatch, "Checkout date is incorrect");
 		softAssert.assertAll();
 	}
 	
